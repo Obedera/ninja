f1db52537fe15d602e1eb3a3e6be0dccdfce71de
Obede <obede.silva7@gmail.com>
2019-08-02 17:58:47 -0300

    Metodo post debug

--- shinobi/views.py
+++ shinobi/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+# from leitor_html import analizar_html
+# from leitor_js import analizar_js
+# from leitor_css import analizar_css
 
 
 # Create your views here.
@@ -7,4 +10,9 @@ def index(request):
     return render(request, 'cadastro.html')
 
 def console(request):
-    return render(request, 'console.html')
+    if request.method == 'POST':
+        html = request.POST.get('debug')
+        print(html)
+        args = {'msg':html}
+        return render(request, 'console.html', args)
+    return render(request, 'console.html',{})
