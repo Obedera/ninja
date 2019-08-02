fe7df3526a9331212d9ab070167e521bd13fe95d
kiiinhos <viniciuscamargo713@gmail.com>
2019-08-02 17:29:42 -0300

    estilizar cadastro

--- shinobi/views.py
+++ shinobi/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'cadastro.html')
 
 def console(request):
     return render(request, 'console.html')
