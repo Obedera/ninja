781858fac2d17121cc5a7e28e89d638892f3b18c
kaio marcos <kaiomarcos2015@outlook.com>
2019-08-03 23:23:49 -0300

    criacao bd, e cadastro

--- shinobi/models.py
+++ shinobi/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+#vai cadastrar uma pessoa
+class Cadastro(models.Model):
+
+    nome = models.CharField(
+        max_length = 255,
+        verbose_name = 'Nome',
+    )
+
+    Sobrenome = models.CharField(
+        max_length = 255,
+        verbose_name = 'Sobrenome',
+    )
+
+    email = models.EmailField(
+        max_length = 255,
+        verbose_name = 'E-mail'
+    )
+    
+    senha = models.CharField(
+        max_length = 10,
+        verbose_name = 'Senha'
+    )
+
+    confirmar_senha = models.CharField(
+        max_length = 10,
+        verbose_name = 'Confirmar Senha'
+    )
+
+    criacao = models.DateTimeField(auto_now=True)
+    ativado = models.BooleanField(default=True)
+    
