34efcc22753b660ba4c6d15705751fb44633c583
Obede <obede.silva7@gmail.com>
2019-08-07 10:42:01 -0300

    Scroll Chat

--- shinobi/static/js/chat.js
+++ shinobi/static/js/chat.js
@@ -49,5 +49,6 @@ function submitFormChat(data){
        let respostaBot = JSON.parse(respostaBot_json)
        document.querySelector('#conversa').value += '\nBot: '+respostaBot.texto;
        document.querySelector('input[name="userResposta"]').value = '';
+       document.querySelector('#conversa').scrollTop = document.querySelector('#conversa').scrollHeight;
     })
 }
