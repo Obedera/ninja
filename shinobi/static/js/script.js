23bdd1bf58611b2f800a34c742ee5c20fd788599
Obede <obede.silva7@gmail.com>
2019-08-06 21:55:04 -0300

    estilo chat

--- shinobi/static/js/script.js
+++ shinobi/static/js/script.js
@@ -1,1 +1,7 @@
 alert('ok')
+function exibirChat(){
+    document.querySelector('.chat textarea').classList.toggle('invisivel');
+    document.querySelector('.chat div input').classList.toggle('invisivel');
+    document.querySelector('.chat div button').classList.toggle('invisivel');   
+}
+exibirChat()
