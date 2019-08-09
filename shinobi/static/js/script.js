0e2dbbe2480f78fcf6dd22010b7f56c825440dbd
Obede <obede.silva7@gmail.com>
2019-08-09 14:19:59 -0300

    Botões mudar tamanho da fonte

--- shinobi/static/js/script.js
+++ shinobi/static/js/script.js
@@ -3,6 +3,7 @@ function exibirChat(){
     document.querySelector('.chat div input').classList.toggle('invisivel');
     document.querySelector('.chat div button').classList.toggle('invisivel');   
 }
+
 function mostrarCadastro(){
     document.querySelector('#login').classList.toggle('invisivel');
     document.querySelector('#cadastro').classList.toggle('invisivel');
@@ -11,4 +12,16 @@ function mostrarCadastro(){
 function mostrarLogin(){
     document.querySelector('#cadastro').classList.toggle('invisivel');
     document.querySelector('#login').classList.toggle('invisivel');
+}
+
+function aumentarFonteConsole(){
+    let tamanhoFonte = Number(document.querySelector('textarea[name="debugtexto"]').style.fontSize.split('pt')[0]);
+    tamanhoFonte += 1;
+    document.querySelector('textarea[name="debugtexto"]').style.fontSize = tamanhoFonte+'pt';
+}
+
+function diminuirFonteConsole(){
+    let tamanhoFonte = Number(document.querySelector('textarea[name="debugtexto"]').style.fontSize.split('pt')[0]);
+    tamanhoFonte -= 1;
+    document.querySelector('textarea[name="debugtexto"]').style.fontSize = tamanhoFonte+'pt';
 }
