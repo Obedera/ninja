ea93ed2902a54a19d4a585ad8c3736b408963780
Obede <obede.silva7@gmail.com>
2019-08-19 16:46:34 -0300

    configurar heroku

--- ninja/settings.py
+++ ninja/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shinobi'
+    'shinobi',
 ]
 
 MIDDLEWARE = [
@@ -119,5 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 django_heroku.settings(locals())
