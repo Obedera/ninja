2eb8c6f5a67c6c2ecc0a04d27c1b6ebe65f7d3e9
Obede <obede.silva7@gmail.com>
2019-09-03 11:12:01 -0300

    add mais palavras no incremento

--- shinobi/static/js/incremento.js
+++ shinobi/static/js/incremento.js
@@ -6,8 +6,8 @@ let posicaoUser
 function substituir(texto, tamanho){
     posicaoUser = areatexto.selectionStart;
     areatexto = areatexto.value.substring(0,posicaoUser-tamanho)+texto+areatexto.value.substring(posicaoUser);
-    console.log((posicaoUser-3)+texto.length);
-    posicaoFinal = (posicaoUser-3)+texto.length;
+    posicaoFinal = (posicaoUser-tamanho)+texto.length;
+    console.log(posicaoFinal);
     return areatexto;
 }
 
@@ -15,8 +15,33 @@ function pegarTecla(event){
     if (event.keyCode == 13){
         areatexto = document.querySelector('textarea[name="debugtexto"]');
         posicaoUser = areatexto.selectionStart;
+
+        if(areatexto.value.substring(posicaoUser-5,posicaoUser) == 'aside'){
+            areatexto.value = substituir('<aside></aside>',5);
+            document.querySelector('textarea[name="debugtexto"]').selectionEnd = posicaoFinal;
+        }
         if(areatexto.value.substring(posicaoUser-3,posicaoUser) == 'div'){
             areatexto.value = substituir('<div></div>',3);
+            document.querySelector('textarea[name="debugtexto"]').selectionEnd = posicaoFinal;
+        }
+        if(areatexto.value.substring(posicaoUser-4,posicaoUser) == 'form'){
+            areatexto.value = substituir('<form></form>',4);
+            document.querySelector('textarea[name="debugtexto"]').selectionEnd = posicaoFinal;
+        }
+        if(areatexto.value.substring(posicaoUser-3,posicaoUser) == 'img'){
+            areatexto.value = substituir('<img alt="" src="">',3);
+            document.querySelector('textarea[name="debugtexto"]').selectionEnd = posicaoFinal;
+        }
+        if(areatexto.value.substring(posicaoUser-4,posicaoUser) == 'main'){
+            areatexto.value = substituir('<main></main>',4);
+            document.querySelector('textarea[name="debugtexto"]').selectionEnd = posicaoFinal;
+        }
+        if(areatexto.value.substring(posicaoUser-7,posicaoUser) == 'section'){
+            areatexto.value = substituir('<section></section>',7);
+            document.querySelector('textarea[name="debugtexto"]').selectionEnd = posicaoFinal;
+        }
+        if(areatexto.value.substring(posicaoUser-6,posicaoUser) == 'select'){
+            areatexto.value = substituir('<select></select>',6);
             document.querySelector('textarea[name="debugtexto"]').selectionEnd = posicaoFinal;
         }
     }
